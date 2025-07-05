858e661b38aa9a4b3aa7d1e9df482c9dede9f13d
tendryAxel <hei.tendry@gmail.com>
2025-07-05 22:35:00 +0300

    chore: todo app input

--- src/main.py
+++ src/main.py
@@ -1,23 +1,36 @@
-from msilib.schema import ControlEvent
-
 import flet as ft
 
 
-def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+class TodoApp(ft.Column):
+    def __init__(self):
+        super().__init__()
 
-    def increment_click(e: ControlEvent):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
+def main(page: ft.Page):
+    task = ft.Text("0", size=50, data=0)
+
+    def increment_click(e):
+        task.value = new_task.value
+        task.update()
 
     page.floating_action_button = ft.FloatingActionButton(
         icon=ft.Icons.ADD, on_click=increment_click
     )
+
+    new_task = ft.TextField("test")
     page.add(
         ft.SafeArea(
             ft.Container(
-                counter,
+                new_task,
+                alignment=ft.alignment.center,
+            ),
+            expand=True,
+        )
+    )
+
+    page.add(
+        ft.SafeArea(
+            ft.Container(
+                task,
                 alignment=ft.alignment.center,
             ),
             expand=True,
