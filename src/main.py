dc447141bd887c90be067b24e7c3ed989b1dc028
tendryAxel <hei.tendry@gmail.com>
2025-07-06 20:52:30 +0300

    chore: use faker to mock data

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
-import random
-
+import faker
 import flet as ft
 
 from components.todo import TodoList, TodoModel
 
 
+faker = faker.Faker()
+
+
 def main(page: ft.Page):
-    todo_list = TodoList([TodoModel(random.random().__str__(), random.random().__str__()) for _ in range(100)])
+    todo_list = TodoList([TodoModel(faker.sentence(10), faker.sentence(100)) for _ in range(100)])
 
     page.add(
         ft.SafeArea(
